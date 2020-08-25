9c05c6fcaa95699a5a06d69625aefbed790c8770
Luram Archanjo <luram.archanjo@hotmail.com.br>
2020-08-25 08:41:29 -0300

    First commit

--- src/main/java/com/example/keycloak/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/keycloak/configuration/SecurityConfiguration.java
@@ -1,22 +1,13 @@
 package com.example.keycloak.configuration;
 
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties oAuth2ResourceServerProperties) {
-        return NimbusJwtDecoder.withJwkSetUri(oAuth2ResourceServerProperties.getJwt().getJwkSetUri()).build();
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
