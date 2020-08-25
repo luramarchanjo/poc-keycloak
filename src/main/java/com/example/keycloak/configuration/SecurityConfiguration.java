b44a1e9dcca060071a52a5802c04ba1be5417a6f
Luram Archanjo <luram.archanjo@hotmail.com.br>
2020-08-24 22:57:15 -0300

    First commit

--- src/main/java/com/example/keycloak/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/keycloak/configuration/SecurityConfiguration.java
@@ -21,7 +21,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorizeRequests -> authorizeRequests
-                .antMatchers(HttpMethod.GET, "/v1/restricted").authenticated()
+                /**
+                 * When this is the case, Resource Server will attempt to coerce these scopes into a list of granted
+                 * authorities, prefixing each scope with the string "SCOPE_".
+                 *
+                 * @see https://docs.spring.io/spring-security/site/docs/current/reference/html5/#oauth2resourceserver-jwt-authorization
+                 */
+                .mvcMatchers(HttpMethod.GET, "/v1/restricted").hasAuthority("SCOPE_user:read")
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 
